10b0dd49afb3d8b780bdd639865865ab07b3a78d
jirasupport <hao.ma@tieto.com>
2013-10-12 17:50:16 +0800

    Version 1.0
    For search thumbnail

---
+++ ResultRender/src/main/java/com/plugin/search/SearchResult.java
@@ -0,0 +1,56 @@
+package com.plugin.search;
+
+import org.apache.velocity.VelocityContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import javax.servlet.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+ 
+ 
+import java.net.URI;
+import java.util.Map;
+
+import com.atlassian.confluence.renderer.radeox.macros.MacroUtils;
+import com.atlassian.templaterenderer.TemplateRenderer;
+import com.atlassian.confluence.util.GeneralUtil;
+import com.atlassian.confluence.util.velocity.VelocityUtils;
+import org.apache.velocity.VelocityContext;
+import org.hamcrest.core.IsNull;
+  
+  
+public class SearchResult extends HttpServlet
+{
+
+    private final TemplateRenderer templateRenderer;
+    private static final String TEMPLATE = "templates/searchresult.vm";
+    private static final String CONTENT = "searchcontent";
+    private static Logger log = LoggerFactory.getLogger(SearchResult.class);
+    
+    public SearchResult(TemplateRenderer templateRenderer)
+    {
+        this.templateRenderer = templateRenderer;
+    }
+         
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+    	response.setContentType("text/html;charset=utf-8");
+  	    Map<String, Object> contextMap = MacroUtils.defaultVelocityContext();
+  	   	templateRenderer.render(TEMPLATE, contextMap, response.getWriter());
+    }
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+    	String value = request.getParameter("searchcontent");
+    	log.warn(value+"Starting====================================================="+value+"Starting=====================================================");
+        response.setContentType("text/html;charset=utf-8");
+  	    Map<String, Object> contextMap = MacroUtils.defaultVelocityContext();
+  	    if(!value.isEmpty()){
+  	    	contextMap.put(CONTENT, value);
+  	    }
+  	    templateRenderer.render(TEMPLATE, contextMap, response.getWriter());
+    }
+}
